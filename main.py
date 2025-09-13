93b89e8fe28f56140dbcf17998d7f868e611d94f
EliseyAg <elisey.tagan@gmail.com>
2025-09-13 15:21:04 +0300

    Added index.html

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
+
+
+app = Flask(__name__, static_folder="templates/static")
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
+
+
+@socketio.on("connect")
+def test_connect():
+    print("Connected")
+    emit("my response", {"data": "Connected"})
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
