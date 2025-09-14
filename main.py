8fd351f7b3db777a0508c42386745093c72da2f5
EliseyAg <elisey.tagan@gmail.com>
2025-09-14 14:23:14 +0300

    Deleted unused functions

--- main.py
+++ main.py
@@ -33,34 +33,6 @@ def test_connect():
     emit("my response", {"data": "Connected"})
 
 
-'''
-def capture_frames():
-    """Capture frames from the default camera and emit them to clients."""
-    cap = cv2.VideoCapture(0)
-
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        return
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Failed to capture frame.")
-            break
-
-        # Encode the frame as JPEG
-        _, buffer = cv2.imencode('.jpg', frame)
-        jpg_as_text = base64.b64encode(buffer).decode('utf-8')
-
-        # Emit the encoded frame to all connected clients
-        socketio.emit("frame", jpg_as_text)
-
-        eventlet.sleep(0.1)
-
-    cap.release()
-'''
-
-
 @socketio.on("frame")
 def receive_image(image):
     # Decode the base64-encoded image data
--- templates/static/camera_script.js
+++ templates/static/camera_script.js
@@ -45,39 +45,4 @@ function capture_frame()
 }
 
 
-var recbtn=document.getElementById("recbutton")
-if(recbtn){
-    recbtn.addEventListener('click',()=>{
-        mediarecorder.start()
-    })
-}
-
-var stopbtn=document.getElementById("stopbutton")
-if(stopbtn){
-    stopbtn.addEventListener('click',()=>{
-        mediarecorder.stop()
-    })
-}
-
-function handleDataAvailable(event) {
-
-    if (event.data.size > 0) {
-        recordedChunks.push(event.data);
-        console.log(event.data)
-    }
-}
-function download() {
-    var blob = new Blob(recordedChunks, {
-        type: 'video/webm'
-    });
-    var url = URL.createObjectURL(blob);
-    var a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style = 'display: none';
-    a.href = url;
-    a.download = 'test.webm';
-    a.click();
-    window.URL.revokeObjectURL(url);
-}
-
 let timerId = setInterval(capture_frame, 35);
