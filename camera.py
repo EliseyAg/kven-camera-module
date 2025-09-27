e96101f7742855fe4dddfc879ca2a83b8cc3babd
EliseyAg <elisey.tagan@gmail.com>
2025-09-27 19:00:39 +0300

    Added camera class (NOT WORKING)

---
+++ camera.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
+import cv2
+
+import numpy as np
+import base64
+
+
+def base64_to_image(base64_string):
+    # Extract the base64 encoded binary data from the input string
+    base64_data = base64_string.split(",")[1]
+    # Decode the base64 data to bytes
+    image_bytes = base64.b64decode(base64_data)
+    # Convert the bytes to numpy array
+    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
+    # Decode the numpy array as an image using OpenCV
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    return image
+
+
+class Camera:
+    def __init__(self):
+        pass
+
+    def test_connect(self):
+        print("Connected")
+        emit("my response", {"data": "Connected"})
+
+    def receive_image(self, b_image):
+        # Decode the base64-encoded image data
+        image = base64_to_image(b_image)
--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 from engineio.payload import Payload
 
-import cv2
-
-import eventlet
-import numpy as np
-
-import base64
-import os
+from camera import Camera
 
 
 Payload.max_decode_packets = 1024
@@ -18,34 +12,13 @@ app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
 
-def base64_to_image(base64_string):
-    # Extract the base64 encoded binary data from the input string
-    base64_data = base64_string.split(",")[1]
-    # Decode the base64 data to bytes
-    image_bytes = base64.b64decode(base64_data)
-    # Convert the bytes to numpy array
-    image_array = np.frombuffer(image_bytes, dtype=np.uint8)
-    # Decode the numpy array as an image using OpenCV
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    return image
-
-
-@socketio.on("connect")
-def test_connect():
-    print("Connected")
-    emit("my response", {"data": "Connected"})
-
-
-@socketio.on("frame")
-def receive_image(image):
-    # Decode the base64-encoded image data
-    image = base64_to_image(image)
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
+    cam = Camera()
+    socketio.on_event('connect', cam.test_connect, namespace='/test')
+    socketio.on_event('frame', cam.receive_image, namespace='/test')
     socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
